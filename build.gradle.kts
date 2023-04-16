de9c0b9fc4940d9aec75a267312fdcb71a5fcfb9
Gary Haines <ghaines@gmail.com>
2023-04-15 20:10:00 -0500

    Java 20

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 group = "net.ghaines"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_17
+java.sourceCompatibility = JavaVersion.VERSION_20
 
 repositories {
 	mavenCentral()
