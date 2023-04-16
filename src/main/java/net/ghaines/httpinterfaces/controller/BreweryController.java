e1bb6b30740dc28b46cb4c0cb297111e4970dfac
Gary Haines <ghaines@gmail.com>
2023-04-15 19:29:16 -0500

    Polish

--- src/main/java/net/ghaines/httpinterfaces/controller/BreweryController.java
+++ src/main/java/net/ghaines/httpinterfaces/controller/BreweryController.java
@@ -1,9 +1,9 @@
 package net.ghaines.httpinterfaces.controller;
 
+import lombok.RequiredArgsConstructor;
 import net.ghaines.httpinterfaces.client.BreweryClient;
 import net.ghaines.httpinterfaces.exception.BreweryException;
 import net.ghaines.httpinterfaces.model.Brewery;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,12 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 public class BreweryController {
 
-    final BreweryClient breweryClient;
-    public BreweryController(BreweryClient breweryClient) {
-        this.breweryClient = breweryClient;
-    }
+    private final BreweryClient breweryClient;
 
     @GetMapping("/brewery/{name}")
     public ResponseEntity<List<Brewery>> byName(@PathVariable(value = "name") String name)
@@ -26,7 +24,7 @@ public class BreweryController {
         if (brewery.isEmpty()) {
             throw new BreweryException("Could not find brewery");
         }
-        return ResponseEntity.status(HttpStatus.OK).body(brewery);
+        return ResponseEntity.ok(brewery);
     }
 
 }
