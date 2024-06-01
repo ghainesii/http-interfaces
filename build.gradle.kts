66831686be66bf40355d5da5eb23701b6d894a32
Gary Haines <ghaines@gmail.com>
2024-05-31 21:23:47 -0500

    Spring Boot 3.3, Java 22

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,15 @@
 plugins {
 	java
-	id("org.springframework.boot") version "3.2.0"
-	id("io.spring.dependency-management") version "1.1.4"
-	id("org.graalvm.buildtools.native") version "0.9.28"
+	id("org.springframework.boot") version "3.3.0"
+	id("io.spring.dependency-management") version "1.1.5"
+	id("org.graalvm.buildtools.native") version "0.10.2"
 }
 
 group = "net.ghaines"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_21
+	sourceCompatibility = JavaVersion.VERSION_22
 }
 
 repositories {
@@ -20,7 +20,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
-	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
